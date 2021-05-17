e47e67700e563fcb6a1a8d10464ef165f8a3429b
sudhakarreddy <sudhakar.kreddy67@gmail.com>
2021-05-17 12:51:54 +0000

    Jenkins message added

--- JavaPractice1.java
+++ JavaPractice1.java
@@ -3,6 +3,8 @@
 public class JavaPractice1 {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Jenkins via Git");
+		System.out.println("Jenkins Job sucessfully Done");
+
 	}
 }
 
